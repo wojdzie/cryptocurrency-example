1db73ed03425bb09b0144c54eb22ff0dd313774b
wojdzie <wojdzi@outlook.com>
2023-04-20 19:01:58 +0200

    Simple blockchain implemented

---
+++ blockchain.py
@@ -0,0 +1,77 @@
+from hashlib import sha256
+
+
+def update_hash(*args):
+    hashing_text = ""
+    h = sha256()
+    for arg in args:
+        hashing_text += str(arg)
+
+    h.update(hashing_text.encode('utf-8'))
+
+    return h.hexdigest()
+
+
+class Block:
+    data = None
+    hash = None
+    nonce = 0
+    previous_hash = "0" * 64
+
+    def __init__(self, data, number=0):
+        self.data = data
+        self.number = number
+
+    def hash(self):
+        return update_hash(self.previous_hash, self.number, self.data, self.nonce)
+
+    def __str__(self):
+        return f"""
+        Block#: {self.number} 
+        Hash: {self.hash()}
+        Previous: {self.previous_hash}
+        Data: {self.data}
+        Nonce: {self.nonce}
+        """
+
+
+class Blockchain:
+    difficulty = 5
+
+    def __init__(self, chain=[]):
+        self.chain = chain
+
+    def add(self, block):
+        self.chain.append(block)
+
+    def remove(self, block):
+        self.chain.remove(block)
+
+    def mine(self, block):
+        try:
+            block.previous_hash = self.chain[-1].hash()
+        except IndexError:
+            pass
+
+        while True:
+            if block.hash()[:self.difficulty] == "0" * self.difficulty:
+                self.add(block)
+                break
+            else:
+                block.nonce += 1
+
+    def is_valid(self):
+        for i in range(1, len(self.chain)):
+            _previous = self.chain[i].previous_hash
+            _current = self.chain[i - 1].hash()
+            if _previous != _current or _current[:self.difficulty] != "0" * self.difficulty:
+                return False
+        return True
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
